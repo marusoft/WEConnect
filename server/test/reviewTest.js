34d6c010b1bf0746def15d9ff0ccb06a191698eb
Alimi Kehinde Marufudeen <alimi.kmaruf@gmail.com>
2018-03-26 18:22:19 +0100

    implement test

--- server/test/reviewTest.js
+++ server/test/reviewTest.js
@@ -6,7 +6,7 @@ import app from '../../app';
 const should = chai.should(); // eslint-disable-line no-unused-var
 chai.use(chaiHttp);
 
-describe('Test for posting review', () => {
+describe('POST /businesses/<businessid>/reviews Add a review for a business ', () => {
   it('Should return 201 if successful', (done) => {
     chai.request(app)
       .post('/api/v1/businesses/2/reviews')
@@ -21,7 +21,7 @@ describe('Test for posting review', () => {
       });
     done();
   });
-  it('Should return 400 if any empty parameters', (done) => {
+  it('Should return 400 if any empty value', (done) => {
     chai.request(app)
       .post('/api/v1/businesses/2/reviews')
       .end((err, res) => {
@@ -31,6 +31,75 @@ describe('Test for posting review', () => {
 
       });
     done();
+  });
+});
+
+describe('GET /businesses/<businessid>/reviews Get all reviews for a business', () => {
+  it('Should return 200 for getting reviews', (done) => {
+    chai.request(app)
+      .get('/api/v1/businesses/1/reviews')
+      .end((err, res) => {
+        res.should.have.status(200);
+      });
+    done();
+  });
+  it('Should return 404 for reviews that does not exist', (done) => {
+    chai.request(app)
+      .get('/api/v1/businesses/85/review')
+      .end((err, res) => {
+        res.should.have.status(404);
+
+      });
+    done();
+  });
+});
+
+describe('API to search all business', () => {
+  it('Should return 200 if successful', (done) => {
+    chai.request(app)
+      .get('/api/v1/businesses')
+      .end((err, res) => {
+        res.should.have.status(200);
+        done();
+      });
+  });
+});
+
+describe('API to search a business', () => {
+  it('Should return 200 if successful', (done) => {
+    chai.request(app)
+      .get('/api/v1/businesses/2')
+      .end((err, res) => {
+        res.should.have.status(200);
+        done();
+      });
+  });
+  it('Should return 404 if business is not found', (done) => {
+    chai.request(app)
+      .get('/api/v1/businesses/88')
+      .end((err, res) => {
+        res.should.have.status(404);
+        done();
+      });
+  });
+});
+
+describe('GET /businesses?location=<location> Filter Business with the specified location ', () => {
+  it('Should return 200 for searching by location', (done) => {
+    chai.request(app)
+      .get('/api/v1/businesses?location=lagos')
+      .end((err, res) => {
+        res.should.have.status(200);
+        done();
+      });
+  });
+  it('Should return 200 for searching by category', (done) => {
+    chai.request(app)
+      .get('/api/v1/businesses?category=education')
+      .end((err, res) => {
+        res.should.have.status(200);
+        done();
+      });
   });
 });
 
